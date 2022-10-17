82fe658f14c2c9f610e4468b729bf812d30cc293
Alessandro41 <a.castellano18@studenti.unisa.it>
2022-10-17 10:36:16 +0200

    completato TicTacToe in Lab_03

--- OOP/Lab_03/Tic-tac-toe/src/TicTacToe.java
+++ OOP/Lab_03/Tic-tac-toe/src/TicTacToe.java
@@ -44,8 +44,76 @@ public class TicTacToe {
             board[i][j] = player;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getWinner() {
-        /* TODO */
+        int xCounter, oCounter;
+
+        for (int i = 0; i < ROWS; i++) {
+            xCounter = 0;
+            oCounter = 0;
+            for (int j = 0; j < COLUMNS; j++) {
+                if(board[i][j].equals(player1))
+                    xCounter++;
+                else if (board[i][j].equals(player2))
+                    oCounter++;
+            }
+            if (xCounter == ROWS) {
+                return player1;
+            }
+            if (oCounter == ROWS) {
+                return player2;
+            }
+
+            xCounter = 0;
+            oCounter = 0;
+            for (int j = 0; j < COLUMNS; j++) {
+                if(board[j][i].equals(player1))
+                    xCounter++;
+                else if (board[j][i].equals(player2))
+                    oCounter++;
+            }
+            if (xCounter == ROWS) {
+                return player1;
+            }
+            if (oCounter == ROWS) {
+                return player2;
+            }
+        }
+
+        xCounter = 0;
+        oCounter = 0;
+        for (int i = 0; i < ROWS; i++) {
+            if (board[i][ROWS - i - 1].equals(player1))
+                xCounter++;
+            else if (board[i][ROWS - i - 1].equals(player2))
+                oCounter++;
+            if (xCounter == ROWS) {
+                return player1;
+            }
+            if (oCounter == ROWS) {
+                return player2;
+            }
+        }
+        xCounter = 0;
+        oCounter = 0;
+        for (int i = 0; i < ROWS; i++) {
+
+            if (board[i][i].equals(player1))
+                xCounter++;
+            else if (board[i][i].equals(player2))
+                oCounter++;
+
+            if (xCounter == ROWS) {
+                return player1;
+            }
+            if (oCounter == ROWS) {
+                return player2;
+            }
+        }
+        return "Nessun vincitore";
     }
 
     public static void main(String[] args) {
@@ -76,7 +144,21 @@ public class TicTacToe {
             if (row >= ROWS || column >= COLUMNS) {
                 System.out.println("Combinazione non valida");
             } else {
-                /* TODO */
+                // Set value on the board
+                game.set(row, column, player);
+
+                // Draw board
+                System.out.println(game);
+
+                // Get winnner
+                System.out.println("Vince: " + game.getWinner());
+
+                // Change player
+                if (player.equals(player1)) {
+                    player = player2;
+                } else {
+                    player = player1;
+                }
             }
 
         } while (row < ROWS && column < COLUMNS);
